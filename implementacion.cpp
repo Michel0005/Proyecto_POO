4d1af793a064446a59c27682e33a086fe6136260
Michel0005 <A01714887@tec.mx>
2025-12-07 14:49:40 -0600

    Add files via upload

--- implementacion.cpp
+++ implementacion.cpp
@@ -1,3 +1,8 @@
+/** Este programa es donde se hace la implementacion de las clases
+ * para la gestion integral de archivos vehiculares **/
+ // Autor: Andre Michel Salgado Hidalgo | A01714887
+ // Fecha:  7 de diciembre de 2025
+
 #include <iostream>
 #include <string>
 #include "automotor.cpp"
@@ -6,89 +11,145 @@
 #include "mantenimiento.cpp"
 #include "conductor.cpp"
 #pragma once 
+
 using namespace std; 
 
+/** Implementacion de los metodos de la clase Automotor **/
 Automotor::Automotor(string m, string mod, int km_i):
 marca(m), modelo(mod), kilometraje(km_i){}; 
 
-void Automotor::registrarViaje(int km_recorridos){
+/** registramos un viaje y actualizamos el kilometraje del vehiculo **/
+/* km_recorridos Kilometros recorridos en el viaje */
+/* No regresa valor */
+void Automotor::registrarViaje(int km_recorridos)
+{
     kilometraje += km_recorridos;
+
     cout << marca << "" << modelo << ":kilometraje actualizado a:"
-    << kilometraje << "km" << endl; 
+        << kilometraje << "km" << endl; 
 }
 
-void Automotor::mostrarDetalles()const{
-cout << "Automotor:" << marca << modelo 
-<< "KM:" << kilometraje << "km."
-<< "year" << year_fabricacion << endl;
+/** mostramos los detalles del vehiculo **/
+/* no recibe parametros */
+/* No regresa valor */
+void Automotor::mostrarDetalles()const
+{
+    cout << "Automotor:" << marca << modelo 
+        << "KM:" << kilometraje << "km."
+        << "year" << yearFabricacion << endl;
 }
 
-void Automotor::registrarMantenimiento(string desc, string fecha){
-    RegristoMantenimiento nuevoRegistro(kilometraje, desc, fecha);
-    historial_mantenimiento.push_back(nuevoRegistro);
+/** registramos un mantenimiento en el historial del vehiculo **/
+/* desc Descripcion del mantenimiento
+   fecha Fecha del mantenimiento */
+/* No regresa valor */
+
+void Automotor::registrarMantenimiento(string desc, string fecha)
+{
+    RegistroMantenimiento nuevoRegistro(kilometraje, desc, fecha);
+    historialMantenimiento.push_back(nuevoRegistro);
+
     cout << "[" << marca 
-    <<" "<< modelo << "]Nuevo mantenimiento Registrado" << endl;
+        <<" "<< modelo << "]Nuevo mantenimiento Registrado" << endl;
 }
 
-void Automotor::mostrarHistorialMantenimiento(){
+/** mostramos el historial de mantenimiento **/
+/* no recibe parametros */
+/* No regresa valor */
+
+void Automotor::mostrarHistorialMantenimiento()
+{
     cout << "Historial de Mantenimiento ("<< marca 
-    <<" " << modelo << ")" << endl;  
-    if (historial_mantenimiento.empty()){
+        <<" " << modelo << ")" << endl;  
+    if (historialMantenimiento.empty()){
         cout << "No existe registro" << endl;
         return; 
     }
-    for (const auto& registro : historial_mantenimiento){
+    for (const auto& registro : historialMantenimiento){
         cout << registro.getDetalles() << endl; 
     }
+
+/* separacion */
     cout <<"--------------------------- "<< endl;   
 }
 
 
+/** Implementacion de los metodos de la clase VehiculoTrabajo **/
+
 VehiculoTrabajo::VehiculoTrabajo(string m, string mod, int km_i,
     float rend, bool hibrido): Automotor(m, mod, km_i),
-    rendimiento_gasolina_km(rend),
+    rendimientoGasolinaKm(rend),
     es_hibrido(hibrido){};
 
-void VehiculoTrabajo::mostrarDetalles()const{
+/** mostramos los detalles del vehiculo **/
+/* no recibe parametros */
+
+void VehiculoTrabajo::mostrarDetalles()const
+{
     cout << "Vehiculo de Trabajo" << endl;
     Automotor::mostrarDetalles();
-    cout <<"rendimiento:" << rendimiento_gasolina_km << "km/L | hibrido"
-    << (es_hibrido ? "Y": "N") << endl;
+
+    cout <<"rendimiento:" << rendimientoGasolinaKm << "km/L | hibrido"
+        << (es_hibrido ? "Y": "N") << endl;
 }
+
+/** calculamos el combustible necesario para una distancia dada **/
+/* distancia_km Distancia en kilometros */
+/* regresa un valor float (cantidad de combustible en litros) */
 
 float VehiculoTrabajo::calcularCombustibleNecesario
-(float distancia_km) const {
-    return distancia_km / rendimiento_gasolina_km;
+(float distancia_km) const 
+{
+    return distancia_km / rendimientoGasolinaKm;
 }
+
+/** Implementacion de los metodos de la clase VehiculoPersonal **/
 
 VehiculoPersonal::VehiculoPersonal(string m, string mod, int km_i, int hp):
-Automotor(m, mod, km_i), potencia_hp(hp){};
+Automotor(m, mod, km_i), potenciaHp(hp){};
 
-void VehiculoPersonal::mostrarDetalles() const {
+/** mostramos los detalles del vehiculo **/
+/* no recibe parametros */
+void VehiculoPersonal::mostrarDetalles() const 
+{
     cout << "Vehiculo Personal" << endl;
+
     Automotor::mostrarDetalles();
-    cout << "potencia:" << potencia_hp << "HP" << endl;
+
+    cout << "potencia:" << potenciaHp << "HP" << endl;
 }
 
-void VehiculoPersonal::ActivarModoDeportivo() const {
+/** activamos el modo deportivo del vehiculo **/
+/* no recibe parametros */
+void VehiculoPersonal::activarModoDeportivo() const 
+{
     cout << marca << "" << modelo << "Personal" << endl;
 }
 
-Conductor::Conductor(int id, string nom):
-id_empleado(id), nombre(nom), vehiculo_asignado(nullptr),
-km_conducidos_total(0){};
+/** Implementacion de los metodos de la clase Conductor **/
 
-void Conductor::asignarVehiculo(Automotor* vehiculo){
-    vehiculo_asignado = vehiculo; 
+Conductor::Conductor(int id, string nom):
+idEmpleado(id), nombre(nom), vehiculoAsignado(nullptr),
+kmConducidosTotal(0){};
+
+
+/** asignamos un vehiculo al conductor **/
+/* vehiculo Puntero al vehiculo a asignar */
+void Conductor::asignarVehiculo(Automotor* vehiculo)
+{
+    vehiculoAsignado = vehiculo; 
     cout << "Vehiculo asignado a:" << nombre << "." << endl; 
 }
 
+/** registramos un trabajo realizado por el conductor **/
+/* km Kilometros recorridos en el trabajo */
 void Conductor::registrarTrabajo(int km){
-    if (vehiculo_asignado) {
-        vehiculo_asignado->registrarViaje(km);
-        km_conducidos_total += km;
-        cout << "conductor" << nombre << "total" << km_conducidos_total <<
-        "km" << endl;
+    if (vehiculoAsignado) {
+        vehiculoAsignado->registrarViaje(km);
+        kmConducidosTotal += km;
+
+        cout << "conductor" << nombre << "total" << kmConducidosTotal <<
+            "km" << endl;
     }
     else {
         cout << "El conductor no tiene vehiculo" << endl;
--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/** Funcion principal donde se ejecuta el programa
+ * demostrando la creacion y el uso de las clases 
+ * Automotor, VehiculoTrabajo, VehiculoPersonal, Conductor **/
+ // Autor: Andre Michel Salgado Hidalgo | A01714887
+ // Fecha:  7 de diciembre de 2025
+
 #include <iostream>
 #include <string> 
 #include <vector> 
@@ -6,25 +12,40 @@
 #include "personal.cpp"
 #include "mantenimiento.cpp"
 #include "implementacion.cpp"
+
 using namespace std;
 
+/** Funcion principal **/
 int main(){
 
+    /* bloque de creacion de vehiculos */
     cout << "Creacion de vehiculos" << endl;
     
     VehiculoTrabajo corolla("Toyota", "Corolla", 500, 22.5, true);
     VehiculoPersonal mustang("Ford", "Mustang", 100, 450); 
 
+    /* mostramos detalles iniciales */
+    cout << "Detalles iniciales" << endl;
     cout << "Resgistro de mantenimento" << endl; 
     corolla.registrarMantenimiento
     ("Cambio de aceite y filtro", "2025-12-03");
+    mustang.registrarMantenimiento
+    ("Alineacion y balanceo", "2025-11-25");
 
+    /* bloque de creacion de conductor */
     cout <<  "Conductor" << endl;
     Conductor james(1714887, "James Sunderland"); 
     james.asignarVehiculo(&corolla);
     james.registrarTrabajo(300);
     corolla.registrarMantenimiento("revision de frenos", "2025-12-12");
+    james.registrarTrabajo(150);
+    james.registrarTrabajo(200);
+    corolla.registrarMantenimiento("Cambio de llantas", "2025-12-20");
+    james.registrarTrabajo(100);
+    cout << "Fin de trabajos" << endl;
 
+    /* mostramos detalles finales */
+    cout << "Detalles finales" << endl;
     cout << "Historial" << endl; 
     corolla.mostrarHistorialMantenimiento();
 
