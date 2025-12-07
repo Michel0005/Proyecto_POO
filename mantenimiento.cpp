9a5ef37ebc92f4a7373e1e7234ce52f32a969527
Michel0005 <A01714887@tec.mx>
2025-12-07 12:26:37 -0600

    Add files via upload

--- mantenimiento.cpp
+++ mantenimiento.cpp
@@ -1,24 +1,38 @@
+/** Definimos la clase RegistroMantenimiento para guardar 
+los datos de un servicio de manteunimiento vehicular **/
+// Autor: Andre Michel Salgado Hidalgo | A01714887
+// Fecha:  7 de diciembre de 2025
+
 #include <iostream>
 #include <string> 
 #include <vector> 
 #pragma once 
+
 using namespace std; 
 
-class RegristoMantenimiento {
+class RegistroMantenimiento {
 private:
-    int kilometraje_mantenimiento;
+    int kilometrajeMantenimiento;
     string descripcion;
     string fecha; 
 
 public:
-    RegristoMantenimiento(int km, string desc, string f):
-    kilometraje_mantenimiento(km), descripcion(desc), fecha(f){
+/** Constructores de la clase RegistroMantenimiento
+ *  km Kilometraje al momento de mantenimiento 
+ *  Descripcion del trabajo realizado
+ *  f Fecha en que se realizo mantenimiento
+ *  return no regresa valor  **/
+
+    RegistroMantenimiento(int km, string desc, string f):
+        kilometrajeMantenimiento(km), descripcion(desc), fecha(f) {
 
     };
 
-    string getDetalles() const{
-    return "[ "+ fecha +" ] KM:" + to_string(kilometraje_mantenimiento)
-        + "  descripcion: " + descripcion; 
+/** obtenemos los detalles del mantenimiento en formato de texto
+ *  Una cadena con la fecha, kilometraje y descripcion **/
+    string getDetalles() const {
+        return "[ " + fecha + " ] KM:" + to_string(kilometrajeMantenimiento)
+            + "  descripcion: " + descripcion;
     }
 
 };
